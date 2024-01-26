dc88e3d5f77eed9a5bdd87f5fe702a6a01873118
yunlinIT <yunlinit@gmail.com>
2024-01-26 16:22:17 +0900

    실행 환경 세팅

---
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UsrHomeController {
+	
+	@RequestMapping("/usr/home/main")
+	@ResponseBody
+	public String showMain() {
+		return "안녕";
+	}
+	
+	
+}
