5a270d2266a050af4e6debba0ade6aadd42bfb6a
yunlinIT <yunlinit@gmail.com>
2024-01-26 17:25:42 +0900

    컨트롤액션

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -14,37 +14,20 @@ public class UsrHomeController {
 	}
 	
 	
-	@RequestMapping("/usr/home/main5")
+	@RequestMapping("/usr/home/setCount")
 	@ResponseBody
-	public String showMain5() {
-		count = 0;
-		return "count의 값이 0으로 초기화 되었습니다.";
+	public String setCount(int count) {	
+		this.count = count;
+		return "count의 값이 " + count + "(으)로 초기화 되었습니다.";
 	}
 	
 
-	@RequestMapping("/usr/home/main4")
+	@RequestMapping("/usr/home/getCount")
 	@ResponseBody
-	public int showMain4() {
+	public int getCount() {
 		return count++;
 	}
 
-	@RequestMapping("/usr/home/main")
-	@ResponseBody
-	public String showMain() {
-		return "안녕하세요";
-	}
-
-	@RequestMapping("/usr/home/main2")
-	@ResponseBody
-	public String showMain2() {
-		return "잘가";
-	}
-
-	@RequestMapping("/usr/home/main3")
-	@ResponseBody
-	public int showMain3() {
-		int a = 1;
-		int b = 2;
-		return a + b;
-	}
+	
+	
 }
