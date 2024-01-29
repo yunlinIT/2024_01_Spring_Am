53c1c5fec3646263467f2e7e12edaadfebf2cc77
yunlinIT <yunlinit@gmail.com>
2024-01-29 13:32:55 +0900

    ArticleService 도입 및 기능 이전

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -1,75 +1,31 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.demo.service.ArticleService;
 import com.example.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
-	int lastArticleId;
-	List<Article> articles;
 
-	// 생성자
+	@Autowired
+	private ArticleService articleService;
+
 	public UsrArticleController() {
-		lastArticleId = 0;
-		articles = new ArrayList<>();
 
-		makeTestData();
 	}
-
-	// 서비스 메서드
-	private void makeTestData() {
-		for (int i = 1; i <= 10; i++) {
-			String title = "제목" + i;
-			String body = "내용" + i;
-
-			writeArticle(title, body);
-		}
-	}
-
-	public Article writeArticle(String title, String body) {
-		int id = lastArticleId + 1;
-		Article article = new Article(id, title, body);
-		articles.add(article);
-		lastArticleId++;
-
-		return article;
-	}
-
-	private void deleteArticle(int id) {
-		Article article = getArticle(id);
-		articles.remove(article);
-	}
-
-	private Article getArticle(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				return article;
-			}
-		}
-		return null;
-	}
-	
-	private void modifyArticle(int id, String title, String body) {
-		Article article = getArticle(id);
-		article.setTitle(title);
-		article.setBody(body);
-		 
-	}
-	
-
 
 	// 액션 메서드
-	
+
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
-	public Object getArticleDetail(int id) {
-		Article article = getArticle(id);
+	public Object getArticleAction(int id) {
+		Article article = articleService.getArticle(id);
 
 		if (article == null) {
 			return id + "번 글은 존재하지 않습니다.";
@@ -82,29 +38,28 @@ public class UsrArticleController {
 	@ResponseBody
 	public Object doModify(int id, String title, String body) {
 
-		Article article = getArticle(id);
+		Article article = articleService.getArticle(id);
 
 		if (article == null) {
 			return id + "번 글은 존재하지 않습니다.";
 		}
 
-		modifyArticle(id, title, body);
+		articleService.modifyArticle(id, title, body);
 
 		return article;
 	}
-
 
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
 	public String doDelete(int id) {
 
-		Article article = getArticle(id);
+		Article article = articleService.getArticle(id);
 
 		if (article == null) {
 			return id + "번 글은 존재하지 않습니다.";
 		}
 
-		deleteArticle(id);
+		articleService.deleteArticle(id);
 
 		return id + "번 글이 삭제 되었습니다";
 	}
@@ -112,13 +67,13 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
-		Article article = writeArticle(title, body);
+		Article article = articleService.writeArticle(title, body);
 		return article;
 	}
 
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
-		return articles;
+		return articleService.articles;
 	}
 }
--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -1,75 +1,17 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.example.demo.vo.Article;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 @Controller
 public class UsrHomeController {
 
-	@RequestMapping("/usr/home/getArticle")
+	@RequestMapping("/usr/home/main")
 	@ResponseBody
-	public Article getArticle() {
-		Article article = new Article(1, "제목1", "내용1"); 
+	public String showMain() {
 
-		return article;
+		return "MAIN!";
 	}
 
-	@RequestMapping("/usr/home/getList")
-	@ResponseBody
-	public List<String> getList() {
-		List<String> list = new ArrayList<>(); // 인덱스 -> 1개의 값
-
-		list.add("철수나이");
-		list.add("영수나이");
-
-		return list;
-	}
-
-	@RequestMapping("/usr/home/getMap")
-	@ResponseBody
-	public Map<String, Object> getMap() {
-		Map<String, Object> map = new HashMap<>();
-
-		map.put("철수나이", 11); // 키 -> 벨류
-		map.put("영수나이", 12);
-
-		return map;
-	}
-
-	@RequestMapping("/usr/home/getDouble")
-	@ResponseBody
-	public double getDouble() {
-		return 3.3;
-	}
-
-	@RequestMapping("/usr/home/getBoolean")
-	@ResponseBody
-	public boolean getBoolean() {
-		return true;
-	}
-
-	@RequestMapping("/usr/home/getString")
-	@ResponseBody
-	public String getString() {
-		return "abc";
-	}
-
-	@RequestMapping("/usr/home/getInt")
-	@ResponseBody
-	public int getInt() {
-		return 1;
-	}
 }
-
