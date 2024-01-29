659e15ae8cbe7f6355b1d9a4d74470339777ffda
yunlinIT <yunlinit@gmail.com>
2024-01-29 15:59:00 +0900

    SQL 일부를 xml로 이전, 설정 필요

--- src/main/java/com/example/demo/repository/ArticleRepository.java
+++ src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -13,13 +13,19 @@ import com.example.demo.vo.Article;
 @Mapper
 public interface ArticleRepository {
 
-	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` = #{body}")
+	@Insert("""
+			INSERT INTO
+			article SET
+			regDate = NOW(),
+			updateDate = NOW(),
+			title = #{title}, `body` = #{body}
+			""")
 	public void writeArticle(String title, String body);
 
 	@Select("SELECT LAST_INSERT_ID()")
 	public int getLastInsertId();
 
-	@Select("SELECT * FROM article WHERE id = #{id}")
+//	@Select("SELECT * FROM article WHERE id = #{id}")
 	public Article getArticle(int id);
 
 	@Delete("DELETE FROM article WHERE id = #{id}")
@@ -28,7 +34,7 @@ public interface ArticleRepository {
 	@Update("UPDATE article SET updateDate = NOW(), title = #{title}, `body` = #{body} WHERE id = #{id}")
 	public void modifyArticle(int id, String title, String body);
 
-	@Select("SELECT * FROM article ORDER BY id DESC")
+//	@Select("SELECT * FROM article ORDER BY id DESC")
 	public List<Article> getArticles();
 
 }
