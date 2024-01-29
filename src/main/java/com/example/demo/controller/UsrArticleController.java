8b393fa6e74b9ae467682a57f8342c204f3abd7f
yunlinIT <yunlinit@gmail.com>
2024-01-29 11:17:30 +0900

    게시물 테스트 데이터 생성, 작성 로직 중복 제거

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -11,32 +11,44 @@ import com.example.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
-	
 	int lastArticleId;
 	List<Article> articles;
-	
+
 	public UsrArticleController() {
 		lastArticleId = 0;
 		articles = new ArrayList<>();
+
+		makeTestData();
 	}
 
-	@RequestMapping("/usr/article/doAdd")
-	@ResponseBody
-	public Article doAdd(String title, String body) {
+	private void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			String title = "제목" + i;
+			String body = "내용" + i;
+
+			writeArticle(title, body);
+		}
+	}
+
+	public Article writeArticle(String title, String body) {
 		int id = lastArticleId + 1;
-		Article article = new Article(id, title, body); 
+		Article article = new Article(id, title, body);
 		articles.add(article);
 		lastArticleId++;
 
 		return article;
 	}
 
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		Article article = writeArticle(title, body);
+		return article;
+	}
+
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
-		
 		return articles;
 	}
-
-
 }
--- src/main/java/com/example/demo/vo/Article.java
+++ src/main/java/com/example/demo/vo/Article.java
@@ -1,5 +1,6 @@
 package com.example.demo.vo;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
