c4469e86ce7d42bcc23b83dca0a70ac006d0e002
yunlinIT <yunlinit@gmail.com>
2024-01-30 14:43:09 +0900

    Ut.isNullOrEmpty

--- src/main/java/com/example/demo/util/Ut.java
+++ src/main/java/com/example/demo/util/Ut.java
@@ -1,27 +1,9 @@
 package com.example.demo.util;
 
-import java.lang.reflect.Array;
-import java.util.Map;
-
 public class Ut {
 
-	public static boolean isNullOrEmpty(Object obj) {
-		if(obj == null) {
-			return true;
-		}
-		
-		if (obj instanceof String) {
-			return ((String) obj).trim().length() == 0;
-		}
-		
-		if (obj instanceof Map) {
-			return ((Map<?, ?>) obj).isEmpty();
-		}
-		
-		if (obj.getClass().isArray()) {
-			return Array.getLength(obj) == 0;
-		}
-		return false;
+	public static boolean isNullOrEmpty(String str) {
+		return str == null || str.trim().length() == 0;
 	}
 
 }
