6585010d7d30c03dbbb05c404b14421fe075e96e
yunlinIT <yunlinit@gmail.com>
2024-01-31 17:40:26 +0900

    JSP 연결/ jsp file은 이전 커밋에 해놈

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -2,16 +2,19 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
 
 	@RequestMapping("/usr/home/main")
-	@ResponseBody
 	public String showMain() {
 
-		return "MAIN!";
+		return "/usr/home/main";
 	}
 
+	@RequestMapping("/")
+	public String showRoot() {
+
+		return "redirect:/usr/home/main";
+	}
 }
--- src/main/webapp/WEB-INF/jsp/usr/home/main.jsp
+++ src/main/webapp/WEB-INF/jsp/usr/home/main.jsp
@@ -1,10 +1,9 @@
-<%@ page language="java" contentType="text/html; charset=EUC-KR"
-    pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>메인 페이지</title>
 </head>
 <body>
 	<h1>MAIN 123</h1>
